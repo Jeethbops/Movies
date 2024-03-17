21bf4a73165713406b451c103ca63720009e1991
Jeethbops <jeethbopannawork@gmail.com>
2024-03-17 19:39:53 +0530

    edyoda cards

--- src/App.js
+++ src/App.js
@@ -3,39 +3,20 @@ import './App.css';
 import react, { Component, PureComponent } from "react";
 import axios from "axios";
 import { useState } from 'react';
+import BlogSection from './BlogSection';
 
 export class App extends Component {
-
-    state = {
-        a: 0
-    }
-
-    Change = () => {
-        this.setState({ a : 0 })
-    }
-
-
-    incer = () => {
-        this.setState({ a: this.state.a + 1 })
-    }
-    decre = () => {
-        this.setState({ a: this.state.a - 1 })
-    }
-
-    render() {
-        return (
+    render(){
+        return(
             <>
-            <div id='buttons'> 
-            <button onClick={() => { this.decre() }} className='btn1'> - </button> <p id='val'>{ this.state.a}</p>
-                <button onClick={() => { this.incer() }}  className='btn1'>+</button>
-                <button onClick={() =>{this.Change()}} className='btn2'>Reset</button>
-            </div>
+            <header>
                 
-
+            </header>
+            <BlogSection/>
             </>
-    )
+        )
     }
-
+    
 
 }
 
---
+++ src/BlogSection.js
@@ -0,0 +1,28 @@
+
+
+
+const BlogSection = ()=>{
+    return(
+    <div id='blogsection'>
+                <div className='cards'>
+                    <div id='imgsection'>
+                    <img  className='blogimg' src='https://edyoda.s3.amazonaws.com/media/blog-images/7_reasons_to_learn_Python_u3lFION.jpg'/>
+                    </div>
+                    <div id='titlesection'>
+                    <h className='title'>Fresh Graduate or..</h>
+                    <p className='author'>Arman Ahmedh 
+                    <span className='date'> | 
+                        05-jul-2019
+                        </span></p>
+                        <div id='despbox'></div>
+                        <p id='description'>If you are a fresh graduate - or new to IT - looking for the next job, you need to read this.</p>
+                        </div>
+                    
+
+                </div>
+                
+            </div>
+    )
+
+}
+export default BlogSection;
