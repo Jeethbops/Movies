4b513b466beb5ed2fe86ead707e20648054476f5
Jeethbops <jeethbopannawork@gmail.com>
2024-04-10 11:02:50 +0530

    final changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import react, { Component, PureComponent } from "react";
 import axios from "axios";
 import { useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import BlogSection from './blogsection';
+import BlogSection from './BlogSection';
 import HeaderSection from './header';
 import FooterSection from './footer';
 import OptionsSection from './options';
@@ -15,7 +15,6 @@ export class App extends Component {
             <>
                 <HeaderSection />
                 <OptionsSection/>
-                {/* <p>diana tution classes</p> */}
 
                 <BlogSection />
                
