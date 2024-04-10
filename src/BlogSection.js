fcbd72390327c7b2f527811d548bca60184fc3d0
Jeethbops <jeethbopannawork@gmail.com>
2024-04-10 10:53:59 +0530

    final changes

--- src/BlogSection.js
+++ src/BlogSection.js
@@ -20,6 +20,136 @@ const BlogSection = ()=>{
                     
 
                 </div>
+                <div className='cards'>
+                    <div id='imgsection'>
+                    <img  className='blogimg' src='https://edyoda.s3.amazonaws.com/media/blog-images/learn_machine_learning_courses_online.png'/>
+                    </div>
+                    <div id='titlesection'>
+                    <h className='title'>Introduction to a Machine Learning </h>
+                    <p className='author'>Arman Ahmedh 
+                    <span className='date'> | 
+                        05-jul-2019
+                        </span></p>
+                        <div id='despbox'></div>
+                        <p id='description'>we have had an insight into artificial Intelligence. well one of its subfeilds is machine learning
+                and this introduction will help you </p>
+                        </div>
+                    
+
+                </div>
+                <div className='cards'>
+                    <div id='imgsection'>
+                    <img  className='blogimg' src='https://edyoda.s3.amazonaws.com/media/blog-images/6.png'/>
+                    </div>
+                    <div id='titlesection'>
+                    <h className='title'>LAMP Stack vs MEAN stack...</h>
+                    <p className='author'>EDYODA
+                    <span className='date'> | 
+                        05-jul-2019
+                        </span></p>
+                        <div id='despbox'></div>
+                        <p id='description'>If you are a fresher - or new to IT - looking for the next job, you need to read this.</p>
+                        </div>
+                    
+
+                </div>
+                <div className='cards'>
+                    <div id='imgsection'>
+                    <img  className='blogimg' src='https://edyoda.s3.amazonaws.com/media/blog-images/4.png'/>
+                    </div>
+                    <div id='titlesection'>
+                    <h className='title'>Impact of Cloud Computing</h>
+                    <p className='author'>Arman Ahmedh 
+                    <span className='date'> | 
+                        05-jul-2019
+                        </span></p>
+                        <div id='despbox'></div>
+                        <p id='description'>If you are a fresher - or new to IT - looking for the next job, you need to read this.</p>
+                        </div>
+                    
+
+                </div>
+                <div className='cards'>
+                    <div id='imgsection'>
+                    <img  className='blogimg' src='https://edyoda.s3.amazonaws.com/media/blog-images/3.png'/>
+                    </div>
+                    <div id='titlesection'>
+                    <h className='title'>Challenges of Machine Learning</h>
+                    <p className='author'>Arman Ahmedh 
+                    <span className='date'> | 
+                        05-jul-2019
+                        </span></p>
+                        <div id='despbox'></div>
+                        <p id='description'>If you are a fresher - or new to IT - looking for the next job, you need to read this.</p>
+                        </div>
+                    
+
+                </div>
+                <div className='cards'>
+                    <div id='imgsection'>
+                    <img  className='blogimg' src='https://edyoda.s3.amazonaws.com/media/blog-images/5.png'/>
+                    </div>
+                    <div id='titlesection'>
+                    <h className='title'>Breaking the Myths Around Cybersecurity</h>
+                    <p className='author'>Arman Ahmedh 
+                    <span className='date'> | 
+                        05-jul-2019
+                        </span></p>
+                        <div id='despbox'></div>
+                        <p id='description'>If you are a fresher - or new to IT - looking for the next job, you need to read this.</p>
+                        </div>
+                    
+
+                </div>
+                <div className='cards'>
+                    <div id='imgsection'>
+                    <img  className='blogimg' src='https://edyoda.s3.amazonaws.com/media/blog-images/7.png'/>
+                    </div>
+                    <div id='titlesection'>
+                    <h className='title'>Importance of data Backup</h>
+                    <p className='author'>Arman Ahmedh 
+                    <span className='date'> | 
+                        05-jul-2019
+                        </span></p>
+                        <div id='despbox'></div>
+                        <p id='description'>If you are a fresher - or new to IT - looking for the next job, you need to read this.</p>
+                        </div>
+                    
+
+                </div>
+                <div className='cards'>
+                    <div id='imgsection'>
+                    <img  className='blogimg' src='https://edyoda.s3.amazonaws.com/media/blog-images/automation-testing-an-overview-2.png'/>
+                    </div>
+                    <div id='titlesection'>
+                    <h className='title'>What is Web Scraping?</h>
+                    <p className='author'>Arman Ahmedh 
+                    <span className='date'> | 
+                        05-jul-2019
+                        </span></p>
+                        <div id='despbox'></div>
+                        <p id='description'>If you are a fresher - or new to IT - looking for the next job, you need to read this.</p>
+                        </div>
+                    
+
+                </div>
+                <div className='cards'>
+                    <div id='imgsection'>
+                    <img  className='blogimg' src='https://edyoda.s3.amazonaws.com/media/blog-images/what_is_isa_blog_1.png'/>
+                    </div>
+                    <div id='titlesection'>
+                    <h className='title'>What is Income Share Agreement</h>
+                    <p className='author'>Arman Ahmedh 
+                    <span className='date'> | 
+                        05-jul-2019
+                        </span></p>
+                        <div id='despbox'></div>
+                        <p id='description'>If you are a fresher - or new to IT - looking for the next job, you need to read this.</p>
+                        </div>
+                    
+
+                </div>
+                
                 
             </div>
     )
