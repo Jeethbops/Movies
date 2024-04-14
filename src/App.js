3a86070bf5e140f729b6660a7d2ea727af7bcb56
Jeethbops <jeethbopannawork@gmail.com>
2024-04-14 11:50:09 +0530

    API

--- src/App.js
+++ src/App.js
@@ -1,30 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import react, { Component, PureComponent } from "react";
+import react, { Component, PureComponent, useEffect } from "react";
 import axios from "axios";
 import { useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import BlogSection from './blogsection';
+import MainSection from './mainsections';
 import HeaderSection from './header';
 import FooterSection from './footer';
 import OptionsSection from './options';
 
-export class App extends Component {
-    render() {
-        return (
-            <>
-                <HeaderSection />
-                <OptionsSection/>
-                <BlogSection />
-               
+function App(){
 
-                <FooterSection/>
-                
-            </>
-        )
-    }
-
-
+    const [poppularmovies,setpopularmovies]= useState([]);
+    useEffect(()=>{
+        axios.get("https://api.themoviedb.org/3/movie/popular?api_key=cfe422613b250f702980a3bbf9e90716")
+        .then
+    })
 }
 
 export default App;
