502622a984d3de61d88f727733d7d3533075f2e5
Jeethbops <jeethbopannawork@gmail.com>
2024-04-14 12:43:59 +0530

    displaying movie cards

--- src/App.js
+++ src/App.js
@@ -4,18 +4,39 @@ import react, { Component, PureComponent, useEffect } from "react";
 import axios from "axios";
 import { useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import MainSection from './mainsections';
-import HeaderSection from './header';
-import FooterSection from './footer';
-import OptionsSection from './options';
 
 function App(){
 
-    const [poppularmovies,setpopularmovies]= useState([]);
+    const [poppularMovies,setPopularmovies]= useState([]);
     useEffect(()=>{
         axios.get("https://api.themoviedb.org/3/movie/popular?api_key=cfe422613b250f702980a3bbf9e90716")
-        .then
-    })
+        .then((res)=>{
+            setPopularmovies(res.data.results)
+        })
+    },[]);
+
+    return(
+        <>
+        <div id='SEARCHBAR'>
+            <input id='searchbar' placeholder='Enter Movie name'></input>
+            <div id='movie-wrapper'>
+                {
+            poppularMovies.map((movie,i) =>{
+                return(
+                    <div className='movie-card'>
+                        <img className='thumbnail'
+                        src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2/${movie.poster_path}` }
+                        />
+
+                    </div>
+                )
+
+            })
+            }
+            </div>
+        </div>
+        </>
+    )
 }
 
 export default App;
